1ece576524e08cc4089129ab67c0f4425eb7293e
a.rahimzadeh <a.rahimzadeh@emofid.com>
2023-01-07 15:48:47 +0330

    Initial commit

---
+++ MobilePrograming-SwiftTrader/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MobilePrograming-SwiftTrader
+//
+//  Created by ata on 07/01/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, Swift!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MobilePrograming-SwiftTrader/MobilePrograming_SwiftTraderApp.swift
@@ -0,0 +1,17 @@
+//
+//  MobilePrograming_SwiftTraderApp.swift
+//  MobilePrograming-SwiftTrader
+//
+//  Created by ata on 07/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct MobilePrograming_SwiftTraderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
