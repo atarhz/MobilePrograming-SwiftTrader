80701686b3a5b8a5fb9c78c225ba173bc63a840a
a.rahimzadeh <a.rahimzadeh@emofid.com>
2023-01-08 02:06:09 +0330

    The profile part of program is done!

--- MobilePrograming-SwiftTrader/ContentView.swift
+++ MobilePrograming-SwiftTrader/ContentView.swift
@@ -8,14 +8,85 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var showSettingsSheet = false
+    @State private var showEditTextSheet = false
+    @State private var isCaps = false
+    @State private var text = "TheProAta"
+    
+    private var displayedText: String {
+        return self.isCaps ? text.uppercased() : text
+    }
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, Swift!")
+            HStack {
+                Spacer()
+                Button("Edit") {
+                    showSettingsSheet.toggle()
+                }
+                .padding()
+                .sheet(isPresented: $showSettingsSheet){
+                    Form{
+                        Section("Settings") {
+                                Button("Change Username") {
+                                    showEditTextSheet.toggle()
+                                }
+                                Button("Done") {
+                                    showSettingsSheet.toggle()
+                                }
+                        }
+                        Section("Preview") {
+                            Text("Current Username: \(Text(displayedText).bold())")
+                        }
+                    }
+                    .sheet(isPresented: $showEditTextSheet){
+                        Form{
+                            Section("Change the text") {
+                                TextField("Edit the current Username", text: $text)
+                                        Button("Submit") {
+                                            showEditTextSheet.toggle()
+                                        }
+                            }
+                        }
+                    }
+                }
+            }
+            
+            Section{
+                
+                HStack{
+                    Text("Username: \(Text(displayedText).bold())")
+                    Spacer()
+                }.padding()
+                
+                Spacer()
+                HStack {
+                    Section{
+                        Spacer()
+                        Button(action: {
+                            print("fu")
+                        }, label: {
+                            VStack {
+                                Image(systemName: "bitcoinsign.circle")
+                                Text("Crypto")
+                            }
+                        })
+                        Spacer()
+                        Spacer()
+                        Button(action: {
+                            print("fu")
+                        }, label: {
+                            VStack {
+                                Image(systemName: "person.circle.fill")
+                                Text("Profile")
+                            }
+                        })
+                    }
+                    Spacer()
+                }.padding().background(Color(red: 0.973, green: 0.973, blue: 0.973)).ignoresSafeArea()
+            }
         }
-        .padding()
     }
 }
 
